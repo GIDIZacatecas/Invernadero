87e2f8e6e05c56c582c0eb32b35f251653874113
majuajo <majuajo@outlook.com>
2016-04-21 12:14:07 -0500

    Create bomba.py

---
+++ actuadores/bomba.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import logging
+import time
+
+from random import randint
+from threading import Thread
+
+class iBomba(object):
+
+    def __init__(self):
+
+        logging.info('Invernadero Bomba de agua')
+
+    def iBombaPrender(self, estado):
+        self.estado = ~ estado
+
+    def iBombaEstado(self):
+        return self.estado
