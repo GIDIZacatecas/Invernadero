691e565781364d4d5ce92767a57fadb1bcf77e75
Abraham Arce <xe1gyq@gmail.com>
2016-04-23 11:49:05 +0000

    Temperatura: Grove Temperature Sensor
    Implement Code to read through Grove Temperature Sensor
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- sensores/itemperatura.py
+++ sensores/itemperatura.py
@@ -3,6 +3,8 @@
 import logging
 import time
 
+import pyupm_grove as grove
+
 from random import randint
 from threading import Thread
 
@@ -13,12 +15,15 @@ class iTemperatura(object):
         logging.info('Invernadero Temperatura')
         self.temperatura = 0
 
+        self.grovetemperature = grove.GroveTemp(0)
+
         thread = Thread(target=self.iTemperaturaActualizar)
         thread.start()
 
     def iTemperaturaActualizar(self):
         while True:
-            self.temperatura = randint(20,40)
+            self.temperature = self.grovetemperature.value()
+            #self.temperatura = randint(20,40)
 
     def iTemperaturaLectura(self):
         return self.temperatura
