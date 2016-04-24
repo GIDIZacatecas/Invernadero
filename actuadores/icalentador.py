5570c170d99da1a7554d1942e74569a93838686e
Abraham Arce <xe1gyq@gmail.com>
2016-04-24 18:33:00 -0500

    iCalentador

--- actuadores/icalentador.py
+++ actuadores/icalentador.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 
 import logging
-import time
-
-from random import randint
-from threading import Thread
+import mraa
 
 class iCalentador(object):
 
     def __init__(self):
-
         logging.info('Invernadero Calentador')
+        self.calentador = mraa.Gpio(3)
+        self.calentador.dir(mraa.DIR_OUT)
+        self.estado = 0
 
     def iCalentadorPrender(self, estado):
-        self.estado = ~ estado
+        self.calentador.write(estado)
 
     def iCalentadorEstado(self):
         return self.estado
